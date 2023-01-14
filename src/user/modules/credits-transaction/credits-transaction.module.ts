1960490deb8ec6d90ca4b516e70ad6bed22b616f
Darkbound <vachev.t@gmail.com>
2023-01-15 00:59:38 +0200

    gitignore

--- src/user/modules/credits-transaction/credits-transaction.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CreditsTransactionService } from "./credits-transaction.service";
-import { CreditsTransactionController } from "./credits-transaction.controller";
-
-@Module({
-  controllers: [CreditsTransactionController],
-  providers: [CreditsTransactionService]
-})
-export class CreditsTransactionModule {}
--- src/user/modules/credits-transaction/dto/update-credits-transaction.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateCreditsTransactionDto } from "./create-credits-transaction.dto";
-
-export class UpdateCreditsTransactionDto extends PartialType(CreateCreditsTransactionDto) {}
